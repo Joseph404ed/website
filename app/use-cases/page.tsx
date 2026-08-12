de285fdb688102e2c259a15192bd6246335f49a4
pycogram <kaluifesinachi@yahoo.com>
2026-08-12 15:17:58 +0100

    feat: update use-cases page structure and presentation

--- app/use-cases/page.tsx
+++ app/use-cases/page.tsx
@@ -3,7 +3,7 @@ import UseCases from './usecase-content';
 
 export const metadata: Metadata = {
   alternates: {
-    canonical: "https://www.zeroicai.xyz/use-case",
+    canonical: "https://www.rustyai.xyz/use-case",
   },
 };
 
